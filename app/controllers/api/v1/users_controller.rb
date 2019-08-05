69704f271610bb1f7aadfdfbeef4b9748e357311
Rose Brennan <rose@grubhub.com>
2019-08-05 17:08:22 -0600

    add user index

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,12 @@
 class Api::V1::UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
 
+  def index
+    @users = User.all
+
+    render json: @users
+  end
+
   # POST /users
   def create
     @user = User.new(user_params)
