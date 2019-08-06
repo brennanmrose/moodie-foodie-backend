d973ee04f0e48f3b37142ec391c424fc3f619987
Rose Brennan <rose@grubhub.com>
2019-08-05 21:51:25 -0600

    nest routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
 
 	namespace :api do
 		namespace :v1 do
-		  resources :moods
-		  resources :desserts
-		  resources :drinks
-		  resources :foods
+		  resources :moods do
+			  resources :desserts
+			  resources :drinks
+			  resources :foods
+			end 
 		end
 	end
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
