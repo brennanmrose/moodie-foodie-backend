c4ba1807473aed8b73197b4be105dcb994c538d2
Rose Brennan <rose@grubhub.com>
2019-08-05 21:22:16 -0600

    add more validations to models

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -1,4 +1,7 @@
 class Drink < ApplicationRecord
 	belongs_to :mood
+
 	validates :mood_id, presence: true
+	validates :name, presence: true
+	validates :description, presence: true
 end
--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,7 @@
 class Food < ApplicationRecord
 	belongs_to :mood
+
 	validates :mood_id, presence: true
+	validates :name, presence: true
+	validates :description, presence: true
 end
