f2b85970901151f3895a4146d9c52d5769d634bc
Rose Brennan <rose@grubhub.com>
2019-08-05 21:34:51 -0600

    add testing data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Mood.create(name: 'Happy')
+Food.create(name: 'Happy food', description: 'happy food desc', mood_id: 1)
+Drink.create(name: 'Happy drink', description: 'happy drink desc', mood_id: 1)
+Dessert.create(name: 'Happy dessert', description: 'happy dessert desc', mood_id: 1)
 
+  # create_table "desserts", force: :cascade do |t|
+  #   t.string "name"
+  #   t.text "description"
+  #   t.integer "mood_id"
+  #   t.datetime "created_at", null: false
+  #   t.datetime "updated_at", null: false
+  # end
+
+  # create_table "drinks", force: :cascade do |t|
+  #   t.string "name"
+  #   t.text "description"
+  #   t.integer "mood_id"
+  #   t.datetime "created_at", null: false
+  #   t.datetime "updated_at", null: false
+  # end
+
+  # create_table "foods", force: :cascade do |t|
+  #   t.string "name"
+  #   t.text "description"
+  #   t.integer "mood_id"
+  #   t.datetime "created_at", null: false
+  #   t.datetime "updated_at", null: false
+  # end
+
+  # create_table "moods", force: :cascade do |t|
+  #   t.string "name"
+  #   t.datetime "created_at", null: false
+  #   t.datetime "updated_at", null: false
+  # end
