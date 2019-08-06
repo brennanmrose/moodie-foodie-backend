9040ec185b64be21b8c786cf6c37f71b4c939bce
Rose Brennan <rose@grubhub.com>
2019-08-05 20:48:32 -0600

    update tables

--- db/migrate/20190806015626_create_dietary_restrictions.rb
+++ db/migrate/20190806015626_create_dietary_restrictions.rb
@@ -6,7 +6,7 @@ class CreateDietaryRestrictions < ActiveRecord::Migration[5.2]
       t.boolean :peanut_free
       t.boolean :gluten_free
       t.boolean :soy_free
-      t.integer :user_id
+      t.integer :user_id, allow_nil: false
 
       t.timestamps
     end
